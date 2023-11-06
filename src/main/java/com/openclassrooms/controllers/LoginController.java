73105c5d9c8bd667bb1aa0acdbda725f5a1b6b55
romainsessa <romainsessa@gmail.com>
2023-11-06 15:59:19 +0100

    API security with Spring Security and JWT

--- src/main/java/com/openclassrooms/controllers/LoginController.java
+++ src/main/java/com/openclassrooms/controllers/LoginController.java
@@ -1,93 +1,24 @@
 package com.openclassrooms.controllers;
 
-import java.security.Principal;
-import java.util.Map;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.services.JWTService;
 
 @RestController
 public class LoginController {
 
-	private OAuth2AuthorizedClientService authorizedClientService;
+	private JWTService jwtService;
 
-	public LoginController(OAuth2AuthorizedClientService authorizedClientService) {
-		this.authorizedClientService = authorizedClientService;
+	public LoginController(JWTService jwtService) {
+		this.jwtService = jwtService;
 	}
 
-	@GetMapping("/user")
-	public String getUser() {
-		return "Welcome, User";
-	}
-
-	@GetMapping("/admin")
-	public String getAdmin() {
-		return "Welcome, Admin";
-	}
-
-	@GetMapping("/*")
-	public String getUserInfo(Principal user, @AuthenticationPrincipal OidcUser oidcUser) {
-		StringBuffer userInfo = new StringBuffer();
-		if (user instanceof UsernamePasswordAuthenticationToken) {
-			userInfo.append(getUsernamePasswordLoginInfo(user));
-		} else if (user instanceof OAuth2AuthenticationToken) {
-			userInfo.append(getOauth2LoginInfo(user, oidcUser));
-		}
-		return userInfo.toString();
-	}
-
-	private StringBuffer getUsernamePasswordLoginInfo(Principal user) {
-		StringBuffer usernameInfo = new StringBuffer();
-
-		UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
-		if (token.isAuthenticated()) {
-			User u = (User) token.getPrincipal();
-			usernameInfo.append("Welcome, " + u.getUsername());
-		} else {
-			usernameInfo.append("NA");
-		}
-		return usernameInfo;
-	}
-
-	private StringBuffer getOauth2LoginInfo(Principal user, OidcUser oidcUser) {
-		StringBuffer protectedInfo = new StringBuffer();
-
-		OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
-		OAuth2AuthorizedClient authClient = this.authorizedClientService
-				.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
-		if (authToken.isAuthenticated()) {
-
-			Map<String, Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
-
-			String userToken = authClient.getAccessToken().getTokenValue();
-			protectedInfo.append("Welcome, " + userAttributes.get("name") + "<br><br>");
-			protectedInfo.append("e-mail: " + userAttributes.get("email") + "<br><br>");
-			protectedInfo.append("Access Token: " + userToken + "<br><br>");
-
-			if (oidcUser != null) {
-				OidcIdToken idToken = oidcUser.getIdToken();
-				if (idToken != null) {
-					protectedInfo.append("idToken value: " + idToken.getTokenValue() + "<br><br>");
-					protectedInfo.append("Token mapped values <br><br>");
-					Map<String, Object> claims = idToken.getClaims();
-					for (String key : claims.keySet()) {
-						protectedInfo.append("  " + key + ": " + claims.get(key) + "<br>");
-					}
-				}
-			}
-		} else {
-			protectedInfo.append("NA");
-		}
-		return protectedInfo;
+	@PostMapping("/login")
+	public String getToken(Authentication authentication) {
+		String token = jwtService.generateToken(authentication);
+		return token;
 	}
 
 }
